ee005d5693f48bff5b375dd46de841eac003dbc9
elgeorsk <elgeor.sk@users.noreply.github.com>
2018-01-07 09:49:08 +0200

    More fixes for Lesson21

--- Lesson21/js/designs.js
+++ Lesson21/js/designs.js
@@ -3,29 +3,38 @@
 
 // When size is submitted by the user, call makeGrid()
 
-const sizePicker = $('#sizePicker');
+const sizePicker = $("#sizePicker");
 
 var i, j;
-var rows, columns;
-var markup = '';
+var rowscolumns;
+var grid = "";
+
+$(function(){
+    $(".canvas h2").hide();
+});
 
 function makeGrid() {
 
 // Your code goes here 
-    
-    rows = $('#input_height').val();
-    columns = $('#input_width').val();
+    $(".canvas h2").show();
+    rows = $("#input_height").val();
+    columns = $("#input_width").val();
     
     for (i = 0; i < rows; i++){
-        markup = "<tr>";
+        grid = "<tr class=\"grid\">";
         for (j = 0; j < columns; j++){
-            markup = markup + "<td></td>";
+            grid = grid + "<td></td>";
         }
-        $('#pixel_canvas').append(markup + "</tr>");
+        $("#pixel_canvas").append(grid + "</tr>");
     }
+    
 }
 
 sizePicker.submit(function(event){
     event.preventDefault();
+    $(".grid").remove();
+    $(".canvas h2").hide();
+    $("h3").remove().before("#pixel_canvas");
     makeGrid();
+    $("<h3>Created Canvas is " + columns + " x " + rows + "</h3>").insertBefore("#pixel_canvas");
 });
